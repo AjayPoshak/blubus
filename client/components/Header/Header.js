419eefcbc05973e5ef3ad3927fcfe784aab75735
Ajay Poshak <poshakajay@gmail.com>
2018-06-02 18:48:45 +0530

    Create app header component

---
+++ client/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from '../logo';
+import HeaderWrapper from './style';
+
+const Header = () => (
+	<HeaderWrapper>
+		<header>
+			<Logo />
+		</header>
+	</HeaderWrapper>
+);
+
+export default Header;
+
