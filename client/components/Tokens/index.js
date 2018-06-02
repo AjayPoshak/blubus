2d4fd0c86ac42017ddf8f5ab729816f8f5ae7367
Ajay Poshak <poshakajay@gmail.com>
2018-06-02 18:49:35 +0530

    Create style tokens for colors, fonts and misc things

---
+++ client/components/Tokens/index.js
@@ -0,0 +1,5 @@
+import colors from './colors';
+import { misc, fonts } from './misc';
+
+export { colors, misc, fonts };
+
---
+++ client/components/Tokens/misc.js
@@ -0,0 +1,22 @@
+export const misc = {
+	input: {
+		height: '56px',
+		width: '100%',
+		icon: {
+			height: '22px',
+			width: '22px',
+		},
+	},
+};
+
+export const fonts = {
+	input: {
+		fontSize: '18px',
+		fontWeight: '400',
+	},
+	button: {
+		fontSize: '18px',
+		fontWeight: '400',
+	},
+};
+
