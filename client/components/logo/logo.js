e25d9769a4640c203b2ea8148d76b6d29287af42
Ajay Poshak <poshakajay@gmail.com>
2018-06-02 18:49:50 +0530

    Create logo component

---
+++ client/components/logo/logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TextWrapper = styled.h1`
+	color: #fff;
+	font-size: 25px;
+	font-weight: 500;
+`;
+
+const Logo = () => (
+	<TextWrapper>BluBus</TextWrapper>
+);
+export default Logo;
