da95095ea8a4323aa735d80b155d3c24d5e222ea
Ajay Poshak <poshakajay@gmail.com>
2018-06-10 10:52:58 +0530

    Update handlers in textinput

--- client/components/TextInput/TextInput.js
+++ client/components/TextInput/TextInput.js
@@ -1,30 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 
 import { InputWrapper, Input, Icon } from './style';
 
-const TextInput = (props) => {
-	const {
-		type, handleChange, icon,
-	} = props;
-	return (
-		<Fragment>
-			<InputWrapper>
-				<Input type={type} onChange={handleChange} {...props} />
-				{icon && <Icon src={icon} alt={icon} />}
-			</InputWrapper>
-		</Fragment>
-	);
-};
+class TextInput extends Component {
+	constructor() {
+		super();
+		this.handleChange = this.handleChange.bind(this);
+	}
+
+	handleChange(event) {
+		this.props.handleChange(event.target.value);
+	}
+
+	render() {
+		const { type, icon } = this.props;
+		return (
+			<Fragment>
+				<InputWrapper>
+					<Input type={type} onChange={this.handleChange} {...this.props} value={this.props.value} />
+					{icon && <Icon src={icon} alt={icon} />}
+				</InputWrapper>
+			</Fragment>
+		);
+	}
+}
 
 TextInput.defaultProps = {
-	icon: '',
+	icon: ''
 };
 
 TextInput.propTypes = {
 	type: PropTypes.oneOf(['text', 'password']).isRequired,
 	handleChange: PropTypes.func.isRequired,
-	icon: PropTypes.string,
+	icon: PropTypes.string
 };
 export default TextInput;
-
