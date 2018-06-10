fadc015287c1020d131c26c4c2978c3e34f05618
Ajay Poshak <poshakajay@gmail.com>
2018-06-10 10:48:26 +0530

    Add route for seatlayout

--- client/routes.js
+++ client/routes.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './views/HomePage';
 import Listing from './views/Listing';
+import SeatLayout from './views/SeatLayout';
 
 /*
 	Switch only renders the first match. Subrouting happens downstream
@@ -13,6 +14,7 @@ const App = () => (
 	<Switch>
 		<Route exact path="/" component={HomePage} />
 		<Route exact path="/listing" component={Listing} />
+		<Route exact path="/seatlayout" component={SeatLayout} />
 	</Switch>
 );
 
