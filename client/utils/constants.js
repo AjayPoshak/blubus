4927c0e2f2f885e57ca0235b14282d01f12987d8
Ajay Poshak <poshakajay@gmail.com>
2018-06-10 18:33:23 +0530

    Add regex for number, mobile and email validation

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -3,3 +3,8 @@ export const LISTING_API_URL = 'http://localhost:3004/listing';
 export const SEATLAYOUT_API_URL = 'http://localhost:3004/seatlayout';
 
 export const MAX_ITEMS_IN_SEARCH_RESULTS = 5;
+
+// PATTERNS
+export const mobilePattern = /^\d{10}$/;
+export const numberPattern = /^\d{1,2}$/;
+export const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
