83470770adfd5a64691c9967d1879987081b5a32
Ajay Poshak <poshakajay@gmail.com>
2018-06-10 22:23:06 +0530

    Update bundle path

--- ssr/helpers.js
+++ ssr/helpers.js
@@ -15,6 +15,6 @@ const renderFullPage = (html, preloadedState) => `<!DOCTYPE html>
 	<script>
 		window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
 	</script>
-	<script src='http://localhost:8080/build/client/main.bundle.js'></script>
+	<script src='/build/client/main.bundle.js'></script>
 	</html>`;
 export default renderFullPage;
