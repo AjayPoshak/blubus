8ac1ca71dfc54614755528fdb40b3f0b70439526
Ajay Poshak <poshakajay@gmail.com>
2018-06-12 05:47:09 +0530

    Update listing API URL

--- client/utils/constants.js
+++ client/utils/constants.js
@@ -1,5 +1,5 @@
 export const HOME_PG_API_URL = 'https://api.myjson.com/bins/13oj0a';
-export const LISTING_API_URL = 'https://api.myjson.com/bins/x4rcq';
+export const LISTING_API_URL = 'https://api.myjson.com/bins/o8l72';
 export const SEATLAYOUT_API_URL = 'https://api.myjson.com/bins/syq4a';
 
 export const MAX_ITEMS_IN_SEARCH_RESULTS = 5;
