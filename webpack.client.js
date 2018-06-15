3af993499417cbda2eb8dbc1e93312b7759016f6
Ajay Poshak <poshakajay@gmail.com>
2018-06-15 19:19:37 +0530

    Create list of plugins based on env it is running

--- webpack.client.js
+++ webpack.client.js
@@ -5,9 +5,43 @@ const path = require('path'),
 	UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 const isProd = process.env.NODE_ENV === 'production';
+/**
+ * Plugins for dev environment
+ */
+const devPlugins = [
+	new HtmlWebpackPlugin({
+		template: './client/index.html',
+		title: 'Bus Booking in Africa'
+	}),
+	new AssetsPlugin({
+		prettyPrint: true,
+		filename: 'assets.json',
+		path: path.resolve(__dirname, 'build')
+	})
+];
+/**
+ * Plugins for production environment
+ */
+const prodPlugins = [
+	new BrotliPlugin({
+		asset: '[path].br[query]',
+		test: /\.(js|css|html|svg)$/,
+		threshold: 10240,
+		minRatio: 0.8
+	}),
+	new UglifyJsPlugin({
+		cache: true,
+		parallel: true,
+		sourceMap: true
+	})
+];
+/**
+ * Merging plugins on the basis of env
+ */
+const pluginList = isProd ? [...devPlugins, ...prodPlugins] : devPlugins;
 
 module.exports = {
-	devtool: isProd ? 'inline-source-map' : '',
+	devtool: isProd ? '' : 'inline-source-map',
 	entry: './client/index.js',
 	output: {
 		filename: '[name].bundle.js',
@@ -23,28 +57,7 @@ module.exports = {
 		historyApiFallback: true,
 		contentBase: path.join(__dirname, 'build/client')
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './client/index.html',
-			title: 'Bus Booking in Africa'
-		}),
-		new AssetsPlugin({
-			prettyPrint: true,
-			filename: 'assets.json',
-			path: path.resolve(__dirname, 'build')
-		}),
-		new BrotliPlugin({
-			asset: '[path].br[query]',
-			test: /\.(js|css|html|svg)$/,
-			threshold: 10240,
-			minRatio: 0.8
-		}),
-		new UglifyJsPlugin({
-			cache: true,
-			parallel: true,
-			sourceMap: true
-		}) // eslint-disable-line
-	],
+	plugins: pluginList,
 	optimization: {
 		splitChunks: {
 			cacheGroups: {
