118bbf3425b1cd27a2760ba9984b84be8a092e8e
Ajay Poshak <poshakajay@gmail.com>
2018-06-30 15:46:04 +0530

    Initialize Raven config for production env only
    We don't want to log dev errors on Raven

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-/** global document */
+/** global document, Raven */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -9,6 +9,15 @@ import configureStore from './store';
 // eslint-disable-next-line no-underscore-dangle
 const preloadedState = window.__PRELOADED_STATE__;
 const store = configureStore(preloadedState); // Initial State can be passed here
+
+// eslint-disable-next-line
+if (__ENV__ !== 'PRODUCTION') {
+	// eslint-disable-next-line
+	Raven.config('https://354e971ff9b945f8a047feea16d6d74e@sentry.io/1235360', {
+		release: '0-0-0',
+		environment: 'production'
+	}).install();
+}
 
 ReactDOM.render(
 	<Provider store={store}>
