660824f434f6aae02af54d045c55bfcc1a336895
Ajay Poshak <poshakajay@gmail.com>
2018-10-19 21:59:21 +0530

    Add name parameter to config to identify the server webpack logs when both client and server webpack are run together

--- webpack.server.js
+++ webpack.server.js
@@ -2,14 +2,15 @@ const path = require('path'),
 	nodeExternals = require('webpack-node-externals');
 
 module.exports = {
+	name: 'server',
 	entry: './ssr/index.js',
 	module: {
-		rules: [{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }],
+		rules: [{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }]
 	},
 	output: {
 		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'build/server'),
+		path: path.resolve(__dirname, 'build/server')
 	},
 	target: 'node', // Tells webpack not to touch any built-in modules like fs or path
-	externals: [nodeExternals()], // Not to bundle node_modules at backend, should be included runtime
+	externals: [nodeExternals()] // Not to bundle node_modules at backend, should be included runtime
 };
