ff53916a658960ab73c3be68a26e92c2ec2fc887
Ajay Poshak <poshakajay@gmail.com>
2018-11-25 15:46:36 +0530

    Add react-loadable for route based code splitting

--- webpack/webpack.client.base.js
+++ webpack/webpack.client.base.js
@@ -1,6 +1,7 @@
 const path = require('path'),
 	webpack = require('webpack'),
 	AssetsPlugin = require('assets-webpack-plugin'),
+	{ ReactLoadablePlugin } = require('react-loadable/webpack'),
 	HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -19,6 +20,9 @@ module.exports = {
 			prettyPrint: true,
 			filename: 'assets.json',
 			path: path.resolve(__dirname, '../build')
+		}),
+		new ReactLoadablePlugin({
+			filename: path.resolve(__dirname, '../build/react-loadable.json')
 		}),
 		new webpack.DefinePlugin({
 			__DEV__: process.env.NODE_ENV === 'production',
