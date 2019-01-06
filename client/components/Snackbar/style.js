ed12777ce9a94506f4a78765c158cbb8a9640841
Ajay Poshak <poshakajay@gmail.com>
2019-01-06 12:09:55 +0530

    Add styles for snackbar feedbacks

---
+++ client/components/Snackbar/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const SnackbarWrapper = styled.section`
+	bottom: 0;
+	width: 95%;
+	color: #fff;
+	padding: 16px;
+	position: fixed;
+	min-width: 250px;
+	background: #000;
+	text-align: left;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+`;
+
+export default SnackbarWrapper;
